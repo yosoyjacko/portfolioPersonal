b4daf8f8e8f20fec38d52b9ebf20dffb72676d7a
jacko <jorge_a.correa@hotmail.com>
2021-05-24 20:02:44 -0300

    certificados al 90%

--- src/components/pages/Cursos.jsx
+++ src/components/pages/Cursos.jsx
@@ -7,19 +7,48 @@ export default function Services() {
         <>
              <div className="container-certification">
                 <div className="content-certification">
-                  <div className="card">
-                      <div className="imgBox">
-                        <img src={IMG2} alt="" />
-                        <img src={IMG1} alt="" />
-                      </div>
-                      <div className="details">
-                          <div className="content">
-                              <h2>Lorem, ipsum<br /></h2>
-                              <h2 className="algo">Lorem, ipsum</h2>
-                          </div>
-                      </div>
-                  </div>
-               
+                    <div className="container-card">
+                        <div className="card">
+                            <div className="face face1">
+                                <div className="content-face1">
+                                    <img src={IMG2} alt="" />
+                                </div>
+                            </div>
+                            <div className="face face2">
+                                <img src={IMG2} alt="" />  
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="face face1">
+                                <div className="content-face1">
+                                    <img src={IMG2} alt="" />
+                                </div>
+                            </div>
+                            <div className="face face2">
+                                <img src={IMG2} alt="" />
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="face face1">
+                                <div className="content-face1">
+                                    <img src={IMG2} alt="" />
+                                </div>
+                            </div>
+                            <div className="face face2">
+                                <img src={IMG2} alt="" />
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="face face1">
+                                <div className="content-face1">
+                                    <img src={IMG2} alt="" />
+                                </div>
+                            </div>
+                            <div className="face face2">
+                                <img src={IMG2} alt="" />
+                            </div>
+                        </div>           
+                    </div>
                 </div>
             
             </div>
