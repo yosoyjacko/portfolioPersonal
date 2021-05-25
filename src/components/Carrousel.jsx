84182fc409f5e63c42b50dcf1011cfaa16802da2
yosoyjacko <jorge_a.correa@hotmail.com>
2021-05-25 14:55:58 -0300

    videos agregados al carousel

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -3,40 +3,52 @@ import React, { Component } from "react";
 import VerticalCarousel from "./VerticalCarousel";
 import uuidv4 from "uuid";
 import { config } from "react-spring";
-import IMG1 from "./../img/foto1.jpg"
+import IMG1 from "./../img/video5.mp4"
+import IMG2 from "./../img/video6.mp4"
+import IMG3 from "./../img/video7.mp4"
+import IMG4 from "./../img/borderSnake.mp4"
+import IMG5 from "./../img/desenfoqueDeFondo.mp4"
+import IMG6 from "./../img/Shinybutton.mp4"
+import IMG7 from "./../img/videoCube3D.mp4"
+import IMG8 from "./../img/video8.mp4"
+import IMG9 from "./../img/video9.mp4"
 
 let slides = [
   {
     key: 1,
-    content: <img src={IMG1} alt=""/>
+    content: <video loop autoPlay muted><source src={IMG1} type="video/mp4" /></video>
   },
   {
     key: 2,
-    content: "2"
+    content: <video loop autoPlay muted><source src={IMG2} type="video/mp4" /></video>
   },
   {
     key: 3,
-    content: "2"
+    content: <video loop autoPlay muted><source src={IMG3} type="video/mp4" /></video>
   },
   {
     key: 4,
-    content: "3"
+    content: <video loop autoPlay muted><source src={IMG4} type="video/mp4" /></video>
   },
   {
     key: 5,
-    content: "4"
+    content: <video loop autoPlay muted><source src={IMG5} type="video/mp4" /></video>
   },
   {
     key: 6,
-    content: "5"
+    content: <video loop autoPlay muted><source src={IMG6} type="video/mp4" /></video>
   },
   {
     key: 7,
-    content: "6"
+    content: <video loop autoPlay muted><source src={IMG7} type="video/mp4" /></video>
   },
   {
     key: 8,
-    content: "7"
+    content: <video loop autoPlay muted><source src={IMG8} type="video/mp4" /></video>
+  },
+  {
+    key: 9,
+    content: <video loop autoPlay muted><source src={IMG9} type="video/mp4" /></video>
   }
 ];
 
