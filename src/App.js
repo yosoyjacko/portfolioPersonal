320f007eee6b0472b3cb0f3bd78a66f75883f207
jacko <jorge_a.correa@hotmail.com>
2021-06-04 21:03:57 -0300

    nose que es

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import styles from "./styles/styles.scss"
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/pages/Home'
 import SobreMi from './components/pages/SobreMi'
-
 import MiCv from './components/pages/MiCv'
 import Proyectos from './components/pages/Proyectos'
 import Cursos from './components/pages/Cursos'
-
 import Navbar2 from "./components/Navbar2";
 
 function App() {
@@ -22,7 +20,6 @@ function App() {
         <Route path='/mi-cv' exact component={MiCv} />
         <Route path='/proyectos' exact component={Proyectos} />
         <Route path='/Certificados' exact component={Cursos} />
-        {/* <Route path='/algo-mas' exact component={AlgoMas} /> */}
       </Switch>
     </Router>
   );
