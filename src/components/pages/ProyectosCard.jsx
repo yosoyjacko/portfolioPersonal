2406c29f912a130593c6d3175c096ced0113e6b0
yosoyjacko <jorge_a.correa@hotmail.com>
2021-06-21 18:32:27 -0300

    creando los links de cada proyecto

--- src/components/pages/ProyectosCard.jsx
+++ src/components/pages/ProyectosCard.jsx
@@ -1,51 +1,61 @@
 import Tarjeta from './Card';
 import '../../styles/ProyectoCards.scss';
-import foto2 from '../../img/foto2.jpg';
-
+import foto1 from '../../img/Sin título.png';
+import foto3 from '../../img/proyecto1.jpg'
+import foto4 from '../../img/proyecto4.jpg'
+import foto5 from '../../img/proyecto2.jpg'
+import foto6 from '../../img/proyecto5.jpg'
+import foto7 from '../../img/proyecto7.jpg'
 
 
 const cards = [
     {
         id: 1,
-        tittle: 'foto1',
-        image: foto2,
-        content:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel iusto accusantium",
-        url: 'https://github.com/yosoyjacko'
+        tittle: 'Fragancias Salas',
+        image: foto1,
+        content:"Landing Page realizado para un local de perfumes y fragancias hecho con html, css, y un poco de php.",
+        url: 'http://fraganciasalas.com/',
+        url2: '#'
     },
     {
         id: 2,
-        tittle: 'foto2',
-        image: foto2,
-        content:"esta es la segunda foto",
-        url: 'https://github.com/yosoyjacko'
+        tittle: 'Movie-App',
+        image: foto3,
+        content:"Buscador de peliculas hecho en React.js utilizando la api de themoviedb.",
+        url: 'https://github.com/yosoyjacko',
+        url2: 'https://github.com/yosoyjacko/movie-app'
     },
     {
         id: 3,
-        tittle: 'foto3',
-        image: foto2,
-        content:"esta es la tercera foto",
-        url: 'https://github.com/yosoyjacko'
+        tittle: 'TreeBank',
+        image: foto4,
+        content:"Billetera virtual creada como proyecto final para el bootcamp de #SOYHENRY. Se realizo con React native y Expo.",
+        url: 'https://drive.google.com/file/d/1ZBBJfk34kLWOKT6QSCeHXc1n09jh6v0S/view',
+        url2: 'https://github.com/yosoyjacko/treebank'
     },
     {
         id: 4,
-        tittle: 'foto1',
-        image: foto2,
-        content:"esta es la cuarta foto",
-        url: 'https://github.com/yosoyjacko'
+        tittle: 'to do list',
+        image: foto5,
+        content:"App para crear listas de tareas. creado con React, Redux y Sass. ",
+        url: 'https://github.com/yosoyjacko',
+        url2: 'https://github.com/yosoyjacko/todo-list'
     },
     {
         id: 5,
-        tittle: 'foto1',
-        image: foto2,
-        content:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel iusto accusantium",
-        url: 'https://github.com/yosoyjacko'
+        tittle: 'Galeria',
+        image: foto6,
+        content:"Practica de una simple galeria con html y css puro",
+        url: 'https://github.com/yosoyjacko',
+        url2: 'https://github.com/yosoyjacko/pruebaFotografo'
     },
     {
         id: 6,
-        tittle: 'foto1',
-        image: foto2,
-        content:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel iusto accusantium",
-        url: 'https://github.com/yosoyjacko'
+        tittle: 'nuts store',
+        image: foto7,
+        content:"Eccomerce creado como proyecto final para el bootcamp de #SOYHENRY. Realizado enteramente con el stack PERN.",
+        url: 'https://github.com/yosoyjacko',
+        url2: 'https://github.com/yosoyjacko/ecommerce-ft07-g13'
     }
 ]
 
@@ -61,6 +71,7 @@ const ProyectosCard = () => {
                                 tittle={card.tittle} 
                                 imageSource={card.image}
                                 url={card.url}
+                                url2={card.url2}
                                 content={card.content}
                             />
                         </div>
